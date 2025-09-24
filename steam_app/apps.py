a8f4906554750baa3718c4f9c67918a331d09bae
Студент группы IS <is@sielom.local>
2025-09-24 11:24:32 +0500

    1 para

--- steam_app/apps.py
+++ steam_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SteamConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'steam'
+    name = 'steam_app'
--- steam_app/views.py
+++ steam_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def hello(request):
+    values = ["dklnh", "pomogite"]
+    return render(request, 
+                  'main.html',
+                  {'values': values,
+                   'is_draw_note': True})
+
+
